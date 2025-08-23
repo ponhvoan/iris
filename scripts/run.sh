f2240d1be0413f05b276589824ba003cfb4f2758
Ponhvoan Srey <ponhvoan.srey@gmail.com>
2025-08-23 20:41:15 +0800

    update script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
-MODELS='meta-llama/Llama-3.2-1B-Instruct'
-DATASETS='helm'
+MODELS='meta-llama/Llama-3.2-3B-Instruct'
+DATASETS='true_false halueval'
 PROMPT='cot'
+PSEUDO_LABEL='verb'
 
 for MODEL in $MODELS
 do
@@ -8,19 +9,16 @@ do
     do
         if [ $DATA = 'true_false' ]; then
             TOPICS='animals cities companies elements facts generated inventions'
-            PSEUDO_LABEL='verb'
         elif [ $DATA = 'halueval' ]; then
             TOPICS='Bio-Medical Education Finance Open-Domain Science'
-            PSEUDO_LABEL='verb'
         elif [ $DATA = 'helm' ]; then
             TOPICS='falcon40b gptj7b llamabase7b llamachat7b llamachat13b opt7b'
-            PSEUDO_LABEL='entropy'
         fi
 
         for TOPIC in $TOPICS
             do
                 python generate_embeddings.py --model $MODEL --dataset_name $DATA --topic $TOPIC --prompt $PROMPT --save_generation --verb
-                python train_classifier.py --model $MODEL --dataset_name $DATA --topic $TOPIC --classifier mlp --num_epochs 10 --prompt $PROMPT --layer 1 --pseudo_label $PSEUDO_LABEL --beta 0.8 --beta2 1 --normalize
+                python train_classifier.py --model $MODEL --dataset_name $DATA --topic $TOPIC --classifier mlp --num_epochs 10 --prompt $PROMPT --layer 1 --pseudo_label $PSEUDO_LABEL --beta 0.8 --phi 1 --normalize
             done
     done
 done
